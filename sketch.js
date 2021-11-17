014dfec2d707b2f5f914d97636ed4a177575ee09
aaronzhang122326 <aaronsu9@gmail.com>
2021-11-17 11:29:40 -0600

    creating 2d array to generate terrain

--- sketch.js
+++ sketch.js
@@ -1,15 +1,26 @@
-// Project Title
-// Your Name
+// CS-30 Major Project 
+// Aaron Su
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = create2DArray();
 }
 
 function draw() {
   background(220);
+}
+
+function create2DArray() {
+  let gridList = [];
+  for (let y = 0; y <= 50; y++) {
+    for (let x = 0; x <=50; x++) {
+      gridList.push(1);
+    }
+  }
+  return gridList;
 }
