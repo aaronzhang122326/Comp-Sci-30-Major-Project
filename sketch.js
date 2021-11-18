0b838cf6ee5b195eff925da50c08b3749f9d6493
aaronzhang122326 <aaronsu9@gmail.com>
2021-11-18 12:11:00 -0600

    working on sanity checks

--- sketch.js
+++ sketch.js
@@ -7,16 +7,17 @@
 let grid;
 let cellSize = 50;
 let playerOne;
+let gridSize = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grid = create2DArray();
+  grid = create2DArray(gridSize, gridSize);
   playerOne = new Player(0, 0, 30, 30, 5);
 }
 
 function draw() {
   background(220);
-  displayGrid(10 ,10);
+  displayGrid(gridSize, gridSize);
 
   //player movement
   playerOne.move();
@@ -25,12 +26,12 @@ function draw() {
 
 
 //creating and displaying grid
-function create2DArray() {
+function create2DArray(row, col) {
   let gridList = [];
-  for (let y = 0; y < 10; y++) {
+  for (let y = 0; y < col; y++) {
     gridList.push([]);
-    for (let x = 0; x < 10; x++) {
-      gridList.push(0);
+    for (let x = 0; x < row; x++) {
+      gridList[y].push(0);
     }
   }
   return gridList;
@@ -38,7 +39,9 @@ function create2DArray() {
 function displayGrid(col, row) {
   for (let y = 0; y < col; y++) {
     for (let x = 0; x < row; x++) {
-      fill("white");
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
@@ -51,17 +54,21 @@ class Player { //player class
     this.width = width;
     this.height = height;
     this.speed = speed;
+    this.rightFree
     this.downFree = true;
+
     this.positionY = floor(this.y/cellSize);
     this.positionX = floor(this.x/cellSize);
   }
   move() {
-    this.positionY = floor(this.y/cellSize);
-    this.positionX = floor(this.x/cellSize);
-    if (grid[this.positionY+1][this.positionX] === 1 && (this.y + this.speed >= (this.positionY+1) * cellSize)) {
-      this.downFree = false;
+    // this.positionY = floor(this.y/cellSize);
+    // this.positionX = floor(this.x/cellSize);
+    // if (grid[this.positionY+1][this.positionX] === 1 && (this.y + this.speed >= (this.positionY+1) * cellSize)) {
+    //   this.downFree = false;
+    // }
+    if (this.x + this.speed >= gridSize * cellSize) {
+      t
     }
-
 
     if (keyIsDown(87)) {
       this.y -= this.speed;
