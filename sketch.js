519cd3e5cf280f56b0c919e4cd0896a971cc4717
aaronzhang122326 <91293784+aaronzhang122326@users.noreply.github.com>
2021-11-19 20:18:28 -0600

    cs 30 final project
    -completed movement sanity check
    -adding functions to randomly generate terrains

--- sketch.js
+++ sketch.js
@@ -7,11 +7,12 @@
 let grid;
 let cellSize = 50;
 let playerOne;
-let gridSize = 10;
+let gridSize = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = create2DArray(gridSize, gridSize);
+  generateRoom(); //change later
   playerOne = new Player(0, 0, 30, 30, 5);
 }
 
@@ -40,6 +41,12 @@ function displayGrid(col, row) {
   for (let y = 0; y < col; y++) {
     for (let x = 0; x < row; x++) {
       if (grid[y][x] === 0) {
+        fill("black");
+      }
+      if (grid[y][x] === 1) {
+        fill("blue");
+      }
+      if (grid[y][x] === 2) {
         fill("white");
       }
       rect(x * cellSize, y * cellSize, cellSize, cellSize);
@@ -70,11 +77,61 @@ class Player { //player class
 
     this.positionY = floor(this.y/cellSize);
     this.positionX = floor(this.x/cellSize);
-    if (this.positionY <= gridSize-1) {
+
+    if (this.positionY <= gridSize-2) { //moving down sanity check
       if (grid[this.positionY+1][this.positionX] === 1 && this.y + this.height + this.speed >= (this.positionY+1) * cellSize) {
         this.downFree = false;
       }
+      if (grid[this.positionY+1][this.positionX+1] === 1 && this.y + this.height + this.speed >= (this.positionY+1) * cellSize && this.x + this.width >= (this.positionX+1) * cellSize) {
+        this.downFree = false;
+      }
     }
+
+    if (this.positionY >= 1) {//moving up sanity check
+      if (grid[this.positionY-1][this.positionX] === 1 && this.y - this.speed <= (this.positionY) * cellSize) {
+        this.upFree = false;
+      }
+      if (grid[this.positionY-1][this.positionX+1] === 1 && this.y - this.speed <= (this.positionY) * cellSize && this.x + this.width >= (this.positionX+1) * cellSize) {
+        this.upFree = false;
+      }
+    }
+
+    
+    if (this.positionX <= gridSize-2) {//moving right sanity check
+      if (this.positionY >= 1) {
+        if (grid[this.positionY][this.positionX+1] === 1 && this.x + this.width + this.speed >= (this.positionX+1) * cellSize) {
+          this.rightFree = false;
+        }
+      }
+      if (this.positionY <= gridSize-2) {
+        if (grid[this.positionY+1][this.positionX+1] === 1 && this.x + this.width + this.speed >= (this.positionX+1) * cellSize && this.y + this.height >= (this.positionY+1) * cellSize) {
+          if (this.y + this.height >= (this.positionY+1) * cellSize) {
+            this.rightFree = false;
+          } 
+        }
+      }
+    }
+
+    if (this.positionX >= 1) {//moving left sanity check
+      if (this.positionY >= 1) {
+        if (grid[this.positionY][this.positionX-1] === 1 && this.x - this.speed <= (this.positionX) * cellSize) {
+          this.leftFree = false;
+        }
+      }
+      if (this.positionY <= gridSize-2) {
+        if (grid[this.positionY+1][this.positionX-1] === 1 && this.x - this.speed <= (this.positionX) * cellSize) {
+          if (this.y + this.height >= (this.positionY+1) * cellSize) {
+            this.leftFree = false;
+          }
+        }
+      }
+    }
+    // if (this.positionY <= gridSize-2) {
+    //   if (this.y + this.height >= (this.positionY+1) * cellSize) {
+    //     this.leftFree = false;
+    //   }
+    // }
+
     if (this.x + this.width + this.speed >= gridSize * cellSize) {
       this.rightFree = false;
     }
@@ -107,3 +164,23 @@ class Player { //player class
     rect(this.x, this.y, this.width, this.height);
   }
 }
+
+function generateRoom() {
+  let roomWidth = random(4,8);
+  let roomHeight = random(4,8);
+  let locationY = 1;
+  let locationX = 1;
+
+  for (let y = locationY; y <= roomHeight; y++) {//walls
+    for (let x = locationX; x <= roomWidth; x++){
+      grid[y][x] = 1;
+    }
+  }
+
+  for (let y = locationY+1; y <= roomHeight-1; y++) {//interior
+    for (let x = locationX+1; x <= roomWidth-1; x++){
+      grid[y][x] = 2;
+    }
+  }
+}
+
