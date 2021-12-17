86e092ef30e275cb995d3d83a0172c6f543e3727
aaronzhang122326 <aaronsu9@gmail.com>
2021-12-17 11:09:43 -0600

    update

--- sketch.js
+++ sketch.js
@@ -279,12 +279,12 @@ class Player { //player class
     //circle(this.x+this.width/2, this.y+this.width/2, 70);
     fill("red");
     if (walkCount < 10) {
-      image(playerImgOne, this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
+      //image(playerImgOne, this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
     }
     if (walkCount > 10) {
-      image(playerImgTwo, this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
+      //image(playerImgTwo, this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
     }
-    //rect(this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
+    rect(this.x + screenMoveX, this.y+ screenMoveY, this.width, this.height);
   }
 
   shoot() { //good code
@@ -309,12 +309,12 @@ class Player { //player class
       image(slashImg, -this.width - 20, -this.height -20, this.width, this.height*2);
       pop();
 
-      //push();
-      //translate(this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2);
-      //rotate(slashAngle + 345);
-      //arc(0, 0, this.height+100, this.width+100, 0, 90);
-      //arc(0, 0, this.height+100, this.width+100, -ARC_ANGLE/2, ARC_ANGLE/2);
-      //pop(); //change later
+      // push();
+      // translate(this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2);
+      // rotate(slashAngle + 345);
+      // arc(0, 0, this.height+100, this.width+100, 0, 90);
+      // arc(0, 0, this.height+100, this.width+100, -ARC_ANGLE/2, ARC_ANGLE/2);
+      // pop(); //change later
 
       // for (let i = 0; i < enemyList.length; i++) { // After rotation, does the width and height change
       //   //console.log("1");
@@ -332,19 +332,19 @@ class Player { //player class
             //console.log("1");
             if (collidePointArc(enemyList[i].x+ screenMoveX, enemyList[i].y+ screenMoveY , this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2, (this.width+100)/2, slashAngle + 345, ARC_ANGLE)){
               console.log("1");
-              enemyList[i].lives -= 1;
+              enemyList[i].lives -= 10;
             }
             else if (collidePointArc(enemyList[i].x+ screenMoveX, enemyList[i].y + enemyList[i].height+ screenMoveY , this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2, (this.width+100)/2, slashAngle + 345, ARC_ANGLE)){
               console.log("2");
-              enemyList[i].lives -= 1;
+              enemyList[i].lives -= 10;
             }
             else if (collidePointArc(enemyList[i].x + enemyList[i].width+ screenMoveX, enemyList[i].y+ screenMoveY , this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2, (this.width+100)/2, slashAngle + 345, ARC_ANGLE)) {
               console.log("3");
-              enemyList[i].lives -= 1;
+              enemyList[i].lives -= 10;
             }
             else if (collidePointArc(enemyList[i].x + enemyList[i].width+ screenMoveX, enemyList[i].y + enemyList[i].height+ screenMoveY , this.x + screenMoveX + this.width/2, this.y + screenMoveY + this.height/2, (this.width+100)/2, slashAngle + 345, ARC_ANGLE)){
               console.log("4");
-              enemyList[i].lives -= 1;
+              enemyList[i].lives -= 10;
             }
           }
         }
